81c5d7d859f6fa3b8f75c89287c077f5d9d3d496
Lyrene Labor <laborlyrene@LyreneMac.local>
2017-09-11 15:09:50 -0400

    Added all 7 counters and overriden 7 callback methods

--- app/src/main/java/course/labs/activitylab/ActivityOne.java
+++ app/src/main/java/course/labs/activitylab/ActivityOne.java
@@ -6,26 +6,44 @@ import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class ActivityOne extends Activity {
 
 		// string for logcat documentation
 		private final static String TAG = "Lab-ActivityOne";
+		int createCount, startCount, resumeCount, pauseCount, stopCount, destroyCount, restartCount;
+		TextView tvCreate, tvStart, tvResume, tvPause, tvStop, tvDestroy, tvRestart;
 
-		// lifecycle counts
-		//TODO: Create 7 counter variables, each corresponding to a different one of the lifecycle callback methods.
-		//TODO:  increment the variables' values when their corresponding lifecycle methods get called.
 		
 		@Override
 		protected void onCreate(Bundle savedInstanceState) {
 			super.onCreate(savedInstanceState);
 			setContentView(R.layout.activity_one);
-			
+
+			tvCreate = (TextView) findViewById(R.id.create);
+			tvStart = (TextView) findViewById(R.id.start);
+			tvResume = (TextView) findViewById(R.id.resume);
+			tvPause = (TextView) findViewById(R.id.pause);
+			tvStop = (TextView) findViewById(R.id.stop);
+			tvDestroy = (TextView) findViewById(R.id.destroy);
+			tvRestart = (TextView) findViewById(R.id.restart);
+
+			if(savedInstanceState != null){
+				createCount = savedInstanceState.getInt("createCount");
+				startCount = savedInstanceState.getInt("startCount");
+				resumeCount = savedInstanceState.getInt("resumeCount");
+				pauseCount = savedInstanceState.getInt("pauseCount");
+				stopCount = savedInstanceState.getInt("stopCount");
+				destroyCount = savedInstanceState.getInt("destroyCount");
+				restartCount = savedInstanceState.getInt("restartCount");
+			}
+
 			//Log cat print out
 			Log.i(TAG, "onCreate called");
-			
-			//TODO: update the appropriate count variable & update the view
+			createCount++;
+			tvCreate.setText("onCreate() calls: " + Integer.toString(createCount));
 		}
 
 		@Override
@@ -43,15 +61,71 @@ public class ActivityOne extends Activity {
 			
 			//Log cat print out
 			Log.i(TAG, "onStart called");
-			
-			//TODO:  update the appropriate count variable & update the view
+			startCount++;
+			tvStart.setText("onStart() calls: " + Integer.toString(startCount));
 		}
 
-	    // TODO: implement 5 missing lifecycle callback methods
+		@Override
+		public void onResume(){
+			super.onResume();
+
+			//Log cat print out
+			Log.i(TAG, "onResume called");
+			resumeCount++;
+			tvResume.setText("onResume() calls " + Integer.toString(resumeCount));
+		}
+
+		@Override
+		public void onPause(){
+			super.onPause();
+
+			//Log cat print out
+			Log.i(TAG, "onPause called");
+			pauseCount++;
+			tvPause.setText("onPause() calls " + Integer.toString(pauseCount));
+		}
+
+		@Override
+		public void onStop(){
+			super.onStop();
+
+			//Log cat print out
+			Log.i(TAG, "onStop called");
+			stopCount++;
+			tvStop.setText("onStop() calls: " + Integer.toString(stopCount));
+		}
+
+		@Override
+		public void onDestroy(){
+			super.onDestroy();
+
+			//Log cat print out
+			Log.i(TAG, "onDestroy called");
+			destroyCount++;
+			tvDestroy.setText("onDestroy() calls: " + Integer.toString(destroyCount));
+		}
+
+		@Override
+		public void onRestart(){
+			super.onRestart();
+
+			//Log cat print out
+			Log.i(TAG, "onRestart called");
+			restartCount++;
+			tvRestart.setText("onRestart() calls: " + Integer.toString(restartCount));
+		}
+
 
 		@Override
 		public void onSaveInstanceState(Bundle savedInstanceState){
-			//TODO:  save state information with a collection of key-value pairs & save all  count variables
+			super.onSaveInstanceState(savedInstanceState);
+			savedInstanceState.putInt("createCount", createCount);
+			savedInstanceState.putInt("startCount", startCount);
+			savedInstanceState.putInt("resumeCount", resumeCount);
+			savedInstanceState.putInt("pauseCount", pauseCount);
+			savedInstanceState.putInt("stopCount", stopCount);
+			savedInstanceState.putInt("destroyCount", destroyCount);
+			savedInstanceState.putInt("restartCount", restartCount);
 		}
 
 
